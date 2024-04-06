1ce8250e3a8e3c322d86d67fd60a81ea80a1b443
Marcin Malinowski <marcin.mm25@gmail.com>
2024-04-06 09:44:35 +0200

    add loading pieces

---
+++ Grafika i komunikacja człowiek-komputer/Chess/loadPices.py
@@ -0,0 +1,18 @@
+import pygame
+
+black_queen = pygame.image.load('assets/black/queen.png')
+black_king = pygame.image.load('assets/black/king.png')
+black_bishop = pygame.image.load('assets/black/bishop.png')
+black_knight = pygame.image.load('assets/black/knight.png')
+black_rook = pygame.image.load('assets/black/rook.png')
+black_pawn = pygame.image.load('assets/black/pawn.png')
+
+white_queen = pygame.image.load('assets/white/queen.png')
+white_king = pygame.image.load('assets/white/king.png')
+white_bishop = pygame.image.load('assets/white/bishop.png')
+white_knight = pygame.image.load('assets/white/knight.png')
+white_rook = pygame.image.load('assets/white/rook.png')
+white_pawn = pygame.image.load('assets/white/pawn.png')
+
+white_field = pygame.image.load('assets/white_field.png')
+black_field = pygame.image.load('assets/black_field.png')
