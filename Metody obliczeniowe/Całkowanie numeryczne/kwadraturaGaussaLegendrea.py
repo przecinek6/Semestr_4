ae014403f1ad9014c141a8666f00c5887673fa3a
Marcin Malinowski <marcin.mm25@gmail.com>
2024-04-25 01:51:01 +0200

    Całkowanie numeryczne kwadratury Gaussa–Legendre’a

---
+++ Metody obliczeniowe/Całkowanie numeryczne/kwadraturaGaussaLegendrea.py
@@ -0,0 +1,31 @@
+import math
+import numpy as np
+
+def wyznacz_wezly_wagi(n):
+    wezly = []
+    wagi = []
+
+    pom = np.polynomial.legendre.leggauss(n)
+    wezly = pom[0]
+    wagi = pom[1]
+
+    return wezly, wagi
+
+def calka(x):
+    return math.sin((0.7 * x) + 0.1) / 1.3 + math.cos(x**2 + 0.2)
+
+def kwadratura(a, b, n):
+    waga, wezel = wyznacz_wezly_wagi(n)
+    suma = 0
+
+    for i in range(n):
+        t = ((b - a)/2) * waga[i] + ((b + a)/2)
+        suma += wezel[i] * calka(t)
+
+    return suma * (b - a) / 2
+
+a = 0.3
+b = 1.1
+n = int(input("Liczba przedzialow: "))
+
+print(kwadratura(a, b, n))
