1d7133ed911f6a784a87855ae6fb25cd18170484
Marcin Malinowski <marcin.mm25@gmail.com>
2024-04-24 23:10:15 +0200

    Całkowanie numeryczne metoda trapezów

---
+++ Metody obliczeniowe/Całkowanie numeryczne/trapezow.py
@@ -0,0 +1,19 @@
+import math
+
+def calka(x):
+    return math.sin((0.7 * x) + 0.1) / 1.3 + math.cos(x**2 + 0.2)
+
+def metoda_trapezow(a, b, n):
+    h = (b - a) / n
+    suma = (calka(a) + calka(b)) / 2
+
+    for i in range(1, n):
+        suma += calka(a + i * h)
+
+    return suma * h
+
+a = 0.3
+b = 1.1
+n = int(input("Liczba przedzialow: "))
+
+print(metoda_trapezow(a, b, n))
