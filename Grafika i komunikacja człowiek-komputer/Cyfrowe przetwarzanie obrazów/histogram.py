2cab72448586a6c2b94baf592fadb46b0874a1dd
Marcin Malinowski <marcin.mm25@gmail.com>
2024-05-08 06:48:19 +0200

    Generowanie histogramu dla składowych R, G i B dostosowane do GUI

--- Grafika i komunikacja człowiek-komputer/Cyfrowe przetwarzanie obrazów/histogram.py
+++ Grafika i komunikacja człowiek-komputer/Cyfrowe przetwarzanie obrazów/histogram.py
@@ -1,34 +1,27 @@
-from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 
 def generate_histogram(image):
-    # Convert image to numpy array
     img_array = np.array(image)
 
-    # Get R, G, B channels
     r_channel = img_array[:, :, 0].flatten()
     g_channel = img_array[:, :, 1].flatten()
     b_channel = img_array[:, :, 2].flatten()
 
-    # Plot histograms
     plt.figure(figsize=(12, 6))
 
-    # Red channel histogram
     plt.subplot(1, 3, 1)
     plt.hist(r_channel, bins=256, color='red', alpha=0.7)
     plt.title('Red Channel')
     plt.xlabel('Pixel Intensity')
     plt.ylabel('Frequency')
 
-    # Green channel histogram
     plt.subplot(1, 3, 2)
     plt.hist(g_channel, bins=256, color='green', alpha=0.7)
     plt.title('Green Channel')
     plt.xlabel('Pixel Intensity')
     plt.ylabel('Frequency')
 
-    # Blue channel histogram
     plt.subplot(1, 3, 3)
     plt.hist(b_channel, bins=256, color='blue', alpha=0.7)
     plt.title('Blue Channel')
@@ -37,7 +30,3 @@ def generate_histogram(image):
 
     plt.tight_layout()
     plt.show()
-
-image = Image.open("Images/flower1.png")
-
-generate_histogram(image)
